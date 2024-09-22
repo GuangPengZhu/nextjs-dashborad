1bf0ce4cc264e6039fd07983276d993eb8baea70
朱广鹏 <5647867+zhu_guang_peng@user.noreply.gitee.com>
2024-09-22 18:03:58 +0800

    myFirstNextJS

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page(){
+    return <li>try give a parent folder to this page</li>;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter,Lusitana,Jacquarda_Bastarda_9} from 'next/font/google'
+
+export const inter = Inter({subsets:['latin']});
+export const lusitana = Lusitana({weight:'400',subsets:["latin"]});
+export const jacquarda_Bastarda_9 = Jacquarda_Bastarda_9({weight:'400',subsets:["latin"]});
